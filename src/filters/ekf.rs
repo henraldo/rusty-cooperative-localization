dc142b77374752d5bb04e66cd3c637abd407d5ad
henraldo <henrysdixon01@gmail.com>
2026-04-06 14:59:43 -0400

    added doc strings to filters

--- src/filters/ekf.rs
+++ src/filters/ekf.rs
@@ -5,9 +5,9 @@ use super::Estimator;
 
 pub struct EKF {
     xhat: SystemState,
-    p: StateCov,
-    q: StateCov,
-    r: MeasCov,
+    P: StateCov,
+    Q: StateCov,
+    R: MeasCov,
     ey: ObservationState,
     omega: StateCov,
 }
@@ -22,113 +22,124 @@ impl EKF {
     pub fn new(x0: SystemState, p0: StateCov, q_filter: StateCov, r_filter: MeasCov) -> Self {
         EKF {
             xhat: x0,
-            p: p0,
-            q: q_filter,
-            r: r_filter,
+            P: p0,
+            Q: q_filter,
+            R: r_filter,
             ey: ObservationState::zeros(),
             omega: omega(),
         }
     }
 
+    /// Returns linearized observation sensativity matrix via calculating Jacobian of nonlinear dynamics model
+    ///
+    /// * `u` - control input vector
     fn jacobian_f(&self, u: &ControlInput) -> StateTransition {
-        let mut f = StateTransition::zeros();
+        let mut F = StateTransition::zeros();
         let theta_g = self.xhat[2];
         let theta_a = self.xhat[5];
         let vg = u[0];
         let va = u[2];
 
-        f[(0,2)] = -vg * theta_g.sin();
-        f[(1,2)] = vg * theta_g.cos();
-        f[(3,5)] = -va * theta_a.sin();
-        f[(4,5)] = va * theta_a.cos();
+        F[(0,2)] = -vg * theta_g.sin();
+        F[(1,2)] = vg * theta_g.cos();
+        F[(3,5)] = -va * theta_a.sin();
+        F[(4,5)] = va * theta_a.cos();
 
-        return f;
+        return F;
     }
 
+    /// Returns linearized observation sensativity matrix via calculating Jacobian of nonlinear measurment model
     fn jacobian_h(&self) -> ObsSensativity {
-        let mut h = ObsSensativity::zeros();
+        let mut H: ObsSensativity = ObsSensativity::zeros();
         let xg = self.xhat[0]; let yg = self.xhat[1];
         let xa = self.xhat[3]; let ya = self.xhat[4];
         let dx = xa - xg; let dy = ya - yg;
         let r2 = (dx * dx) + (dy * dy);
         let r = r2.sqrt();
 
-        if r2 < 1e-12 { return h; }
+        if r2 < 1e-12 { return H; }
 
         // UGV bearing row
         let d_bear_g_dx = -dy / r2;
         let d_bear_g_dy = dx / r2;
-        h[(0,0)] = d_bear_g_dx;
-        h[(0,1)] = d_bear_g_dy;
-        h[(0,2)] = -1.0;
-        h[(0,3)] = -d_bear_g_dx;
-        h[(0,4)] = -d_bear_g_dy;
+        H[(0,0)] = d_bear_g_dx;
+        H[(0,1)] = d_bear_g_dy;
+        H[(0,2)] = -1.0;
+        H[(0,3)] = -d_bear_g_dx;
+        H[(0,4)] = -d_bear_g_dy;
 
         // range row
         let d_range_dx = -dx / r;
         let d_range_dy = -dy /r;
-        h[(1,0)] = d_range_dx;
-        h[(1,1)] = d_range_dy;
-        h[(1,3)] = -d_range_dx;
-        h[(1,4)] = -d_range_dy;
+        H[(1,0)] = d_range_dx;
+        H[(1,1)] = d_range_dy;
+        H[(1,3)] = -d_range_dx;
+        H[(1,4)] = -d_range_dy;
 
         // UAV bearing row
         let d_bear_a_dx = dy / r2;
         let d_bear_a_dy = -dx / r2;
-        h[(2,0)] = d_bear_a_dx;
-        h[(2,1)] = d_bear_a_dy;
-        h[(2,3)] = -d_bear_a_dx;
-        h[(2,4)] = -d_bear_a_dy;
-        h[(2,5)] = -1.0;
+        H[(2,0)] = d_bear_a_dx;
+        H[(2,1)] = d_bear_a_dy;
+        H[(2,3)] = -d_bear_a_dx;
+        H[(2,4)] = -d_bear_a_dy;
+        H[(2,5)] = -1.0;
 
         // UAV position rows
-        h[(3,3)] = 1.0;
-        h[(4,4)] = 1.0;
+        H[(3,3)] = 1.0;
+        H[(4,4)] = 1.0;
 
-        return h;
+        return H;
     }
 }
 
 impl Estimator for EKF {
 
-    // Predict states and covariance for next measurement update
+    /// Predict states and covariance for next measurement update
+    ///
+    /// * `u` - control input vector
     fn predict(&mut self, u: &ControlInput) {
         // Propagate nonlinear system dynamics model
         self.xhat = rk4_step_integrator(&self.xhat, u, DT);
 
         // Linearize and predict covariance
-        let f = self.jacobian_f(u);
-        self.p = f * self.p * f.transpose() + (self.omega * self.q * self.omega.transpose());
+        let F: StateTransition = self.jacobian_f(u);
+        self.P = F * self.P * F.transpose() + (self.omega * self.Q * self.omega.transpose());
     }
 
-    // Corrects predictions from latest measurements
+    /// Corrects state predictions from latest measurements
+    ///
+    /// * `z` - latest measurements
     fn correct(&mut self, z: &ObservationState) {
-        let h_til = self.jacobian_h();
-        let yhat = h(&self.xhat);
+        let H: ObsSensativity = self.jacobian_h();
+        let yhat: ObservationState = h(&self.xhat);
 
         self.ey = z - yhat;
         self.ey[0] = wrap_to_pi(self.ey[0]);
         self.ey[2] = wrap_to_pi(self.ey[2]);
 
-        let s = h_til * self.p * h_til.transpose() + self.r;
-        let k = self.p * h_til.transpose() * s.try_inverse().expect("S invertable");
+        let S = H * self.P * H.transpose() + self.R;
+        let K = self.P * H.transpose() * S.try_inverse().expect("S invertable");
 
-        self.xhat += k * self.ey;
+        self.xhat += K * self.ey;
         self.xhat[2] = wrap_to_pi(self.xhat[2]);
         self.xhat[5] = wrap_to_pi(self.xhat[5]);
 
-        let i = StateCov::identity();
-        self.p = (i - k * h_til) * self.p * (i - k * h_til).transpose() + k * self.r * k.transpose();
+        let I: StateCov = StateCov::identity();
+        self.P = (I - K * H) * self.P * (I - K * H).transpose() + K * self.R * K.transpose();
     }
 
+    /// Returns last computed state estimates
     fn get_estimated_state(&self) -> SystemState { self.xhat }
 
+    /// Returns last computed measurement errors
     fn get_measurement_residuals(&self) -> ObservationState { self.ey }
 
+    /// Returns last computed filter covariance diagonal
     fn get_covariance_diagonal(&self) -> SystemState {
-        let mut diag = SystemState::zeros();
+        let mut diag: SystemState = SystemState::zeros();
         for i in 0..N_STATES {
-            diag[i] = self.p[(i,i)];
+            diag[i] = self.P[(i,i)];
         }
         return diag;
     }
--- src/filters/ukf.rs
+++ src/filters/ukf.rs
@@ -12,11 +12,20 @@ pub struct UKF {
     xhat: SystemState, ey: ObservationState, sp_x: SigmaPoints,
     P: StateCov, Q: StateCov, R: MeasCov,
     weights_mean: Weights, weights_covar: Weights,
-    alpha: f64, beta: f64, kappa: f64, lambda: f64,
-    n: usize, L: usize,
+    alpha: f64, beta: f64, kappa: f64,
+    L: usize,
 }
 
 impl UKF {
+    /// Instantiates Unscented Kalman Filter object
+    ///
+    /// * `x0` - inital states
+    /// * `p0` - initial filter covariance
+    /// * `q_filter` - filter process noise covariance
+    /// * `r_filter` - filter measurement noise covariance
+    /// * `a` - alpha parameter for unscented transform
+    /// * `b` - beta parameter for unscented transform
+    /// * `k` - kappa parameter for unscented transform
     pub fn new(
         x0: SystemState,
         p0: StateCov,
@@ -38,18 +47,24 @@ impl UKF {
             alpha: a,
             beta: b,
             kappa: k,
-            lambda: 0.0,
-            n: N_STATES,
             L: (2 * N_STATES) + 1,
         }
     }
 
-    fn update_weights(&mut self) {
-        self.lambda = (self.alpha * self.alpha) * (self.n as f64 + self.kappa) - self.n as f64;
+    /// Calculates filter lambda scaling value from UKF configuration
+    fn calculate_lambda(&self) -> f64 {
+        let n: f64 = N_STATES as f64;
+        return (self.alpha * self.alpha) * (n + self.kappa) - n;
+    }
 
-        self.weights_mean[0] = self.lambda / (self.n as f64 + self.lambda);
+    /// Updates mean and covariance weighting values
+    fn update_weights(&mut self) {
+        let n: f64 = N_STATES as f64;
+        let lambda: f64 = self.calculate_lambda();
+
+        self.weights_mean[0] = lambda / (n + lambda);
         self.weights_covar[0] = self.weights_mean[0] + (1.0 - (self.alpha * self.alpha) + self.beta);
-        let w: f64 = 1.0 / (2.0 * (self.n as f64 + self.lambda));
+        let w: f64 = 1.0 / (2.0 * (n + lambda));
 
         for i in 1..self.L as usize {
             self.weights_mean[i] = w;
@@ -57,30 +72,34 @@ impl UKF {
         }
     }
 
+    /// Generates new set of Sigma points from the latest state estimates and filter covariance
     fn compute_sigma_points(&mut self) {
+        let n: f64 = N_STATES as f64;
         self.update_weights();
 
-        let n: f64 = self.n as f64;
-
-        let gamma: f64 = (n + self.lambda).sqrt();
+        let gamma: f64 = (n + self.calculate_lambda()).sqrt();
         let svp: StateCov = self.P.cholesky().expect("P must be PSD").l();
 
         self.sp_x.column_mut(0).copy_from(&self.xhat);
-        for i in 0..self.n {
+        for i in 0..N_STATES {
             let col: SystemState = gamma * svp.column(i);
 
             self.sp_x.column_mut(i + 1).copy_from(&(self.xhat + col));
             self.sp_x[(2, i)] = wrap_to_pi(self.sp_x[(2, i)]);
             self.sp_x[(5, i)] = wrap_to_pi(self.sp_x[(5, i)]);
 
-            self.sp_x.column_mut(i + 1 + self.n).copy_from(&(self.xhat - col));
-            self.sp_x[(2, i + 1 + self.n)] = wrap_to_pi(self.sp_x[(2, i + 1 + self.n)]);
-            self.sp_x[(5, i + 1 + self.n)] = wrap_to_pi(self.sp_x[(5, i + 1 + self.n)]);
+            self.sp_x.column_mut(i + 1 + N_STATES).copy_from(&(self.xhat - col));
+            self.sp_x[(2, i + 1 + N_STATES)] = wrap_to_pi(self.sp_x[(2, i + 1 + N_STATES)]);
+            self.sp_x[(5, i + 1 + N_STATES)] = wrap_to_pi(self.sp_x[(5, i + 1 + N_STATES)]);
         }
     }
 }
 
 impl Estimator for UKF {
+
+    /// Predict states and covariance for next measurement update
+    ///
+    /// * `u` - control input vector
     fn predict(&mut self, u: &ControlInput) {
         self.compute_sigma_points();
 
@@ -107,6 +126,9 @@ impl Estimator for UKF {
         self.P += self.Q;
     }
 
+    /// Corrects state predictions from latest measurements
+    ///
+    /// * `z` - latest measurements
     fn correct(&mut self, z: &ObservationState) {
         let mut sp_y: SigmaPointsY = SigmaPointsY::zeros();
 
@@ -126,7 +148,7 @@ impl Estimator for UKF {
         self.ey[0] = wrap_to_pi(self.ey[0]);
         self.ey[2] = wrap_to_pi(self.ey[2]);
 
-        let mut Pyy = MeasCov::zeros();
+        let mut Pyy: MeasCov = MeasCov::zeros();
         let mut d_yy: ObservationState = ObservationState::zeros();
         for j in 0..self.L {
             d_yy.copy_from(&sp_y.column(j));
@@ -139,8 +161,8 @@ impl Estimator for UKF {
         Pyy += self.R;
 
         let mut Pxy = na::SMatrix::<f64, N_STATES, N_MEASUREMENTS>::zeros();
-        let mut dx = SystemState::zeros();
-        let mut dy =  ObservationState::zeros();
+        let mut dx: SystemState = SystemState::zeros();
+        let mut dy: ObservationState = ObservationState::zeros();
         for i in 0..self.L {
             dx.copy_from(&self.sp_x.column(i));
             dx -= self.xhat;
@@ -164,8 +186,13 @@ impl Estimator for UKF {
 
     }
 
+    /// Returns last computed state estimates
     fn get_estimated_state(&self) -> SystemState { self.xhat }
+
+    /// Returns last computed measurement errors
     fn get_measurement_residuals(&self) -> ObservationState { self.ey }
+
+    /// Returns last computed filter covariance diagonal
     fn get_covariance_diagonal(&self) -> SystemState {
         let mut diag = SystemState::zeros();
         for i in 0..N_STATES {
